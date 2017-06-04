543572e60c3d2420500b72b95596cee6c9ba989c
satoru <satorulogic@gmail.com>
2017-06-04 09:39:39 +0800

    Config bumpversion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='context.py',
-    version='0.1',
+    version='0.1.0',
     description='A library that create and maintain context stacks',
     author='Satoru Logic',
     author_email='satorulogic@gmail.com',
